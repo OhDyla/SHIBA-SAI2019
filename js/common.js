947e6f3e4b65ab62e212b022cb6289d4d34a383c
KenOHD <al19078@shibaura-it.ac.jp>
2020-10-27 18:38:02 +0900

    append_menu-button

---
+++ js/common.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.menu-btn').on('click', function () {
+        $('.menu').fadeToggle(200);
+    })
+    $('.menu').on('click', function () {
+        $('.menu').fadeToggle(200);
+    })
+});
